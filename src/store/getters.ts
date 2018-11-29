0307aa5d8bab0b2a1a0224d9ef01bb0050a01cea
connor <c.stroomberg@gmail.com>
2018-11-29 11:51:06 +0100

    fix build, add missing close } tag

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -33,15 +33,15 @@ export default {
         return node
       }
       const filteredChildren: TopicNode[] = node.children
-        .map(filter)
-        .filter(isDefined as TermGuard<TopicNode>)
+      .map(filter)
+      .filter(isDefined as TermGuard<TopicNode>)
       const filteredDataItems: string[] = node.dataItems
-        .map(id => state.allDataItems[id])
-        .filter(isDefined as TermGuard<DataItem>)
-        .filter(dataItem => matches(dataItem.label))
-        .map(dataItem => dataItem.id)
+      .map(id => state.allDataItems[id])
+      .filter(isDefined as TermGuard<DataItem>)
+      .filter(dataItem => matches(dataItem.label))
+      .map(dataItem => dataItem.id)
       if (filteredChildren.length || filteredDataItems.length) {
-        return { ...node, children: filteredChildren, dataItems: filteredDataItems }
+        return {...node, children: filteredChildren, dataItems: filteredDataItems}
       }
       return undefined
     }
@@ -65,10 +65,10 @@ export default {
     const selectedSexGroups = new Set(state.selectedOptions.sexGroup)
     const selectedSubCohorts = new Set(state.selectedOptions.subCohorts)
     return (item: DataItem): boolean =>
-      item.sexGroups.some(sexGroup => selectedSexGroups.has(sexGroup.id)) &&
-      item.subCohorts.some(subCohort => selectedSubCohorts.has(subCohort.id)) &&
-      item.collectionPoints.some(collectionPoint => selectedCollectionPoints.has(collectionPoint.id))
-        // && item.ageGroups.some(ageGroup => selectedAgeGroups.has(ageGroup.id))
+        item.sexGroups.some(sexGroup => selectedSexGroups.has(sexGroup.id)) &&
+        item.subCohorts.some(subCohort => selectedSubCohorts.has(subCohort.id)) &&
+        item.collectionPoints.some(collectionPoint => selectedCollectionPoints.has(collectionPoint.id))
+    // && item.ageGroups.some(ageGroup => selectedAgeGroups.has(ageGroup.id))
   },
   dataItemSelected: (state: ApplicationState): DataItemPredicate => {
     const selectedDataItems = new Set(state.selectedDataItems)
@@ -80,26 +80,26 @@ export default {
       return []
     }
     return selectedTopic.dataItems
-        .map((id: string): (DataItem | undefined) => state.allDataItems[id])
-        .filter(isDefined as TermGuard<DataItem>)
+    .map((id: string): (DataItem | undefined) => state.allDataItems[id])
+    .filter(isDefined as TermGuard<DataItem>)
   },
   vueDataItems: (state: ApplicationState, getters: Getters): VueDataItem[] =>
-    getters.topicDataItems
-    .map(item => ({
-      ...item,
-      enabled: getters.dataItemEnabled(item),
-      selected: getters.dataItemSelected(item)
-    })),
+      getters.topicDataItems
+      .map(item => ({
+        ...item,
+        enabled: getters.dataItemEnabled(item),
+        selected: getters.dataItemSelected(item)
+      })),
   selectedAgeGroups: (state: ApplicationState): string[] => state.selectedOptions.ageGroup,
   selectedSexGroups: (state: ApplicationState): string[] => state.selectedOptions.sexGroup,
   selectedCollectionPoints: (state: ApplicationState): string[] => state.selectedOptions.collectionPoint,
   selectedSubCohorts: (state: ApplicationState): string[] => state.selectedOptions.subCohorts,
   selectionCount: (state: ApplicationState, getters: Getters): number => {
     return Object.values(state.allDataItems)
-      .filter(isDefined as TermGuard<DataItem>)
-      .filter(getters.dataItemEnabled)
-      .filter(getters.dataItemSelected)
-      .length
+    .filter(isDefined as TermGuard<DataItem>)
+    .filter(getters.dataItemEnabled)
+    .filter(getters.dataItemSelected)
+        .length
   },
   selectedTree: (state: ApplicationState, getters: Getters): any => {
     const filter = (node: TopicNode): any | undefined => {
@@ -111,10 +111,11 @@ export default {
       .filter(isDefined as TermGuard<DataItem>)
       .filter(dataItem => getters.dataItemSelected(dataItem))
       if (filteredChildren.length || filteredDataItems.length) {
-        return { ...node, children: [...filteredChildren, ...filteredDataItems] }
+        return {...node, children: [...filteredChildren, ...filteredDataItems]}
       }
       return undefined
     }
     return state.topicTree.map(filter).filter(isDefined as TermGuard<any>)
   },
-  searchTerm:  (state: ApplicationState): string => state.selectedOptions.searchTerm
+  searchTerm: (state: ApplicationState): string => state.selectedOptions.searchTerm
+}
